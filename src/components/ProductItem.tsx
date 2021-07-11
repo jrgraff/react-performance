5d0a25269f6044a84f92c72a679a5865c9ef31be
Jackson Graff <jacksongraff@hotmail.com>
2021-07-11 11:21:51 -0300

    useMemo

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -19,10 +19,3 @@ function ProductItemComponent({ product }: ProductItemProps) {
 export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
   return Object.is(prevProps.product, nextProps.product)
 })
-
-/** When to use memo
- * 1. Pure Functional Components
- * 2. Renders too often
- * 3. Re-renders with same props
- * 4. Medium to big size
- */
