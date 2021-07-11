9b0f90c6513e7816cfaa4bb989c1bbcbacaad097
Jackson Graff <jacksongraff@hotmail.com>
2021-07-11 11:56:31 -0300

    data formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,24 @@ import React, { FormEvent, useCallback, useState } from "react"
 
 import { SearchResults } from "../components/SearchResults";
 
+type Product = {
+  id: number;
+  price: number;
+  priceFormatted: string;
+  title: string;
+}
+
+type ResultsProps = {
+  totalPrice: number;
+  data: Product[];
+}
+
 export default function Home() {
   const [search, setSearch] = useState('');
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState<ResultsProps>({
+    totalPrice: 0,
+    data: []
+  });
 
   async function handleSearch(event: FormEvent) {
     event.preventDefault();
@@ -14,9 +29,27 @@ export default function Home() {
     }
 
     const response = await fetch(`http://localhost:3333/products?q=${search}`)
-    const data = await response.json();
+    const data: Product[] = await response.json();
 
-    setResults(data);
+    const formatter = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    })
+
+    const products = data.map(product => {
+      return {
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        priceFormatted: formatter.format(product.price)
+      }
+    })
+
+    const totalPrice = data.reduce((total: number, product: Product) => {
+        return total + product.price
+      }, 0)
+
+    setResults({ totalPrice, data: products });
   }
 
   const onAddToWishlist = useCallback(async (id: number) => {
@@ -40,7 +73,11 @@ export default function Home() {
         <button type="submit">Buscar</button>
       </form>
 
-      <SearchResults results={results} onAddToWishlist={onAddToWishlist} />
+      <SearchResults
+        results={results.data}
+        totalPrice={results.totalPrice}
+        onAddToWishlist={onAddToWishlist} 
+      />
     </div>
   )
 }
