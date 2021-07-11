7edbdfad0b5d03a07266e33b3b0108e70a2381af
Jackson Graff <jacksongraff@hotmail.com>
2021-07-11 11:33:49 -0300

    useCallback

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -7,9 +7,10 @@ interface SearchResultsProps {
     price: number;
     title: string;
   }>
+  onAddToWishlist: (id: number) => void;
 }
 
-export function SearchResults({ results }: SearchResultsProps) {
+export function SearchResults({ results, onAddToWishlist }: SearchResultsProps) {
   const totalPrice = useMemo(() => {
     return results.reduce((total, product) => {
       return total + product.price
@@ -20,14 +21,18 @@ export function SearchResults({ results }: SearchResultsProps) {
    * 1. Heavy calculation
    * 2. Referential equality (when passing information to a child component)
    */
-
+  
   return (
     <div>
       <h2>{totalPrice}</h2>
 
       {results.map(product => {
         return (
-          <ProductItem key={product.id} product={product} />
+          <ProductItem
+            key={product.id}
+            product={product}
+            onAddToWishlist={onAddToWishlist} 
+          />
         )
       })}
     </div>
